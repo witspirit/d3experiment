ebc2428325258d3f00ac7add2b1a462318c299c2
witspirit <witspirit@gmail.com>
2014-05-15 21:47:21 +0200

    Bar Tuturial, Part 3: Introduction of the data.map function and the ordinal scale.

--- resources/js/bar_part3.js
+++ resources/js/bar_part3.js
@@ -1,6 +1,9 @@
 var width = 700;
 var height = 350;
 
+// Automatically space our columns, snap to pixel (round) and pad (percentage) between bars
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+
 var y = d3.scale.linear().range([height, 0]); // We don't know the size of the domain yet !
 
 // In the column chart, we are going to fix both the width and height up front
@@ -11,22 +14,21 @@ var chart = d3.select(".chart").
 // Let's load the data from a separate file
 d3.csv("data/bar_part2.csv", type, function(error, data) {
     // Once the data has arrived, we can compute the data dependent parts
+    x.domain(data.map(function(d) { return d.name; }));
     y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-    var barWidth = width / data.length;
 
     var bar = chart.selectAll("g").
         data(data).
         enter().append("g").
-        attr("transform", function(d,i) { return "translate("+i*barWidth+", 0)"; });
+        attr("transform", function(d,i) { return "translate("+x(d.name)+", 0)"; });
 
     bar.append("rect").
         attr("y", function(d) { return y(d.value); }).
         attr("height", function(d) { return height - y(d.value); }).
-        attr("width", barWidth-1);
+        attr("width", x.rangeBand());
 
     bar.append("text").
-        attr("x", barWidth / 2).
+        attr("x", x.rangeBand() / 2).
         attr("y", function(d) { return y(d.value) + 3; }).
         attr("dy", "0.75em").
         text(function(d) { return d.value; });
