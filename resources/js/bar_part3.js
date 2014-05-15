f02696306982da4f360fe0af79c9ff755f85c7fe
witspirit <witspirit@gmail.com>
2014-05-15 22:13:48 +0200

    Bar Tuturial, Part 3: Adding D3 axis components, replacing our embedded text label.

--- resources/js/bar_part3.js
+++ resources/js/bar_part3.js
@@ -4,8 +4,11 @@ var height = 350 - margin.top - margin.bottom;
 
 // Automatically space our columns, snap to pixel (round) and pad (percentage) between bars
 var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-
 var y = d3.scale.linear().range([height, 0]); // We don't know the size of the domain yet !
+
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+
 
 // In the column chart, we are going to fix both the width and height up front
 var chart = d3.select(".chart").
@@ -14,28 +17,32 @@ var chart = d3.select(".chart").
     append("g").
     attr("transform", "translate("+margin.left+", "+margin.top+")");
 
-
 // Let's load the data from a separate file
 d3.csv("data/bar_part2.csv", type, function(error, data) {
     // Once the data has arrived, we can compute the data dependent parts
     x.domain(data.map(function(d) { return d.name; }));
     y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    var bar = chart.selectAll("g").
-        data(data).
-        enter().append("g").
-        attr("transform", function(d,i) { return "translate("+x(d.name)+", 0)"; });
+    // The axes are added AFTER the scales have their domain added
+    // otherwise they cannot properly determine their labels
+    chart.append("g").
+        attr("class", "x axis").
+        attr("transform", "translate(0, " + height+ ")").
+        call(xAxis);
 
-    bar.append("rect").
+    chart.append("g").
+        attr("class", "y axis").
+        call(yAxis);
+
+    // Bars only now, since the text labels are now part of the axis
+    chart.selectAll(".bar").
+        data(data).
+        enter().append("rect").
+        attr("class", "bar").
+        attr("x", function(d) { return x(d.name); }).
         attr("y", function(d) { return y(d.value); }).
         attr("height", function(d) { return height - y(d.value); }).
         attr("width", x.rangeBand());
-
-    bar.append("text").
-        attr("x", x.rangeBand() / 2).
-        attr("y", function(d) { return y(d.value) + 3; }).
-        attr("dy", "0.75em").
-        text(function(d) { return d.value; });
 });
 
 // Small conversion function to transform to number if possible
