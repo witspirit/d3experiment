3b7214d1e63d492284066f87521d2c393e4d76dd
witspirit <witspirit@gmail.com>
2014-05-24 22:53:48 +0200

    General Update Tutorial, part 3 Animation to illustrate what happens in the various sections

--- resources/js/genupdate1.js
+++ resources/js/genupdate1.js
@@ -15,25 +15,51 @@ function update(data) {
 
     // UPDATE
     // Update old elements as needed
-    text.attr("class", "update");
+    text.attr("class", "update").
+        transition().
+            delay(325). // Some delay to ensure we see the exit first
+            duration(750).
+            attr("x", function(d, i) { return i * 32; });
+    // Above: Move the updated letters to their new positions
 
     // ENTER
     // Create new elements as needed
     text.enter().append("text").
         attr("class", "enter").
         text(function(d) { return d; }).// Since the element and datum now remain matched via the key function, it doesn't change on update anymore, so we only need to set it on enter
-        attr("dy", ".35em");
+        attr("dy", ".35em").
+        // Place the new letters on the correct position. We don't combine it with the update anymore, since we want the update transition effect
+        attr("x", function(d,i) { return i*32; }).
+        // Going to make the new letters 'drop in'
+        attr("y", -60). // So we start a bit on top
+        style("fill-opacity", 1e-6). // And initially transparent
+        transition().
+            // And we transition to their target location
+            delay(975). // Wait until remove and update animation have completed
+            duration(750).
+            attr("y", 0).
+            style("fill-opacity", 1);
 
     // ENTER + UPDATE
     // Appending to the enter selection expands the update selection to include
     // entering elements; so, operations on the update selection after appending to
     // the enter selection will apply to both entering and updating nodes.
-    text.attr("x", function(d, i) { return i * 32; });
+    // No more combination, since we choose 2 visualizations for the update/enter cases
+    // text.attr("x", function(d, i) { return i * 32; });
     // Before the key function, the elements content was rewritten, but now we actually move them, since we keep the content stable
+
+
 
     // EXIT
     // Remove old elements as needed.
-    text.exit().remove();
+    text.exit().
+        attr("class", "exit").
+        transition().
+            // Create a drop out, by moving them down on exit until we fully remove them
+            duration(750).
+            attr("y", 60).
+            style("fill-opacity", 1e-6).
+            remove();
 }
 
 // The initial display
@@ -42,7 +68,7 @@ update(alphabet);
 // Grab a random sample of letters from the alphabet, in alphabetical order.
 setInterval(function() {
     update(shuffle(alphabet).slice(0, Math.floor(Math.random() * 26)).sort());
-}, 1500);
+}, 2000);
 
 // Shuffles the input array.
 function shuffle(array) {
